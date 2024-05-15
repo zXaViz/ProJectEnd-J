8d54f76eb655c450d6c3fd8efec4b09e671959cd
TunKiet <ttk.17092003@gmail.com>
2024-05-15 22:15:29 +0700

    commit

--- ProjectEnd_J/Source/resources/views/home.blade.php
+++ ProjectEnd_J/Source/resources/views/home.blade.php
@@ -13,7 +13,7 @@
                             <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                                 <div class="layer-slick1 animated visible-false" data-appear="fadeInDown" data-delay="0">
                             <span class="ltext-101 cl2 respon2">
-                                HOT 2021
+                                HOT 2024
                             </span>
                                 </div>
 
